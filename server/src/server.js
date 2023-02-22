57a48709d4704eadf4e6f7d404985dc1d71a1310
Kan <mreast1203@gmail.com>
2023-02-22 12:15:22 +0800

    deploy v_1

--- server/src/server.js
+++ server/src/server.js
@@ -6,9 +6,18 @@ import dotenv from "dotenv-defaults";
 const app = express();
 dotenv.config();
 
-app.use(cors());
+if (process.env.NODE_ENV === "development") {
+  app.use(cors());
+}
 app.use(express.json());
 routes(app);
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 4000;
 
