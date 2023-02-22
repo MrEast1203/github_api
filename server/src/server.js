79055772e7a77a9bd0929960fab472cf2c457e0e
Kan <mreast1203@gmail.com>
2023-02-22 12:22:13 +0800

    add path

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import routes from "./routes";
 import dotenv from "dotenv-defaults";
+import path from "path";
 
 const app = express();
 dotenv.config();
